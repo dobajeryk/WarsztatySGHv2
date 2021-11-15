f111bfef719099d0f4a6e9ad80e9e71c0ce54d65
Eryk Dobaj <dobajeryk@gmail.com>
2021-11-15 22:42:35 +0100

    Initial commit

---
+++ src/main/java/com/skni/warsztatysghv2/WarsztatySgHv2Application.java
@@ -0,0 +1,13 @@
+package com.skni.warsztatysghv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsztatySgHv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarsztatySgHv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/skni/warsztatysghv2/WarsztatySgHv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skni.warsztatysghv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarsztatySgHv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
